a08c84f29c5babda14c56601f31dfb731bf6ca73
Jim Lavin <jlavin@jimlavin.net>
2013-04-24 12:58:16 -0500

    Added attribute translation directive to solution

--- src/localize.js
+++ src/localize.js
@@ -8,8 +8,10 @@
  *
  */
 
-angular.module('localization', []).
-    factory('localize', ['$http', '$rootScope', '$window', '$filter', function ($http, $rootScope, $window, $filter) {
+angular.module('localization', [])
+    // localization service responsible for retrieving resource files from the server and
+    // managing the translation dictionary
+    .factory('localize', ['$http', '$rootScope', '$window', '$filter', function ($http, $rootScope, $window, $filter) {
         var localize = {
             // use the $window service to get the language of the user's browser
             language:$window.navigator.userLanguage || $window.navigator.language,
@@ -18,6 +20,7 @@ angular.module('localization', []).
             // flag to indicate if the service hs loaded the resource file
             resourceFileLoaded:false,
 
+            // success handler for all server communication
             successCallback:function (data) {
                 // store the returned array in the dictionary
                 localize.dictionary = data;
@@ -27,11 +30,13 @@ angular.module('localization', []).
                 $rootScope.$broadcast('localizeResourcesUpdates');
             },
 
+            // allows setting of language on the fly
             setLanguage: function(value) {
                 localize.language = value;
                 localize.initLocalizedResources();
             },
 
+            // loads the language resource file from the server
             initLocalizedResources:function () {
                 // build the url to retrieve the localized resource file
                 var url = '/i18n/resources-locale_' + localize.language + '.js';
@@ -44,6 +49,7 @@ angular.module('localization', []).
                 });
             },
 
+            // checks the dictionary for a localized resource string
             getLocalizedString: function(value) {
                 // default the result to an empty string
                 var result = '';
@@ -70,12 +76,20 @@ angular.module('localization', []).
 
         // return the local instance when called
         return localize;
-    } ]).
-    filter('i18n', ['localize', function (localize) {
+    } ])
+    // simple translation filter
+    // usage {{ TOKEN | i18n }}
+    .filter('i18n', ['localize', function (localize) {
         return function (input) {
             return localize.getLocalizedString(input);
         };
-    }]).directive('i18n', ['localize', function(localize){
+    }])
+    // translation directive that can handle dynamic strings
+    // updates the text value of the attached element
+    // usage <span data-i18n="TOKEN" ></span>
+    // or
+    // <span data-i18n="TOKEN|VALUE1|VALUE2" ></span>
+    .directive('i18n', ['localize', function(localize){
         var i18nDirective = {
             restrict:"EAC",
             updateText:function(elm, token){
@@ -109,4 +123,41 @@ angular.module('localization', []).
         };
 
         return i18nDirective;
+    }])
+    // translation directive that can handle dynamic strings
+    // updates the attribute value of the attached element
+    // usage <span data-i18n-attr="TOKEN|ATTRIBUTE" ></span>
+    // or
+    // <span data-i18n-attr="TOKEN|ATTRIBUTE|VALUE1|VALUE2" ></span>
+    .directive('i18nAttr', ['localize', function (localize) {
+        var i18NAttrDirective = {
+            restrict: "EAC",
+            updateText:function(elm, token){
+                var values = token.split('|');
+                // construct the tag to insert into the element
+                var tag = localize.getLocalizedString(values[0]);
+                // update the element only if data was returned
+                if ((tag !== null) && (tag !== undefined) && (tag !== '')) {
+                    if (values.length > 2) {
+                        for (var index = 2; index < values.length; index++) {
+                            var target = '{' + (index - 2) + '}';
+                            tag = tag.replace(target, values[index]);
+                        }
+                    }
+                    // insert the text into the element
+                    elm.attr(values[1], tag);
+                }
+            },
+            link: function (scope, elm, attrs) {
+                scope.$on('localizeResourcesUpdated', function() {
+                    i18NAttrDirective.updateText(elm, attrs.i18nAttr);
+                });
+
+                attrs.$observe('i18nAttr', function (value) {
+                    i18NAttrDirective.updateText(elm, value);
+                });
+            }
+        };
+
+        return i18NAttrDirective;
     }]);
